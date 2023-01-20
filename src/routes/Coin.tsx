e01fe58f38358bab955e4d4a2edfb5bb2aef119f
bpthess <bpthess@naver.com>
2023-01-20 18:35:27 +0900

    api type setting

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -28,24 +28,101 @@ interface RouteState {
   name: string;
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  isNew: boolean;
+  isActive: boolean;
+  type: string;
+  logo: string;
+  tags: object;
+  team: object;
+  description: string;
+  message: string;
+  openSource: boolean;
+  startedAt: string;
+  developmentStatus: string;
+  hardwareWallet: boolean;
+  proofType: string;
+  orgStructure: string;
+  hashAlgorithm: string;
+  links: object;
+  linksExtended: object;
+  whitepaper: object;
+  firstDataAt: string;
+  lastDataAt: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulatingSupply: number;
+  totalSupply: number;
+  maxSupply: number;
+  betaValue: number;
+  firstDataAt: string;
+  lastUpdated: string;
+  quotes: {
+    USD: {
+      athDate: string;
+      athPrice: number;
+      marketCap: number;
+      marketCapChange24h: number;
+      percentChange1h: number;
+      percentChange1y: number;
+      percentChange6h: number;
+      percentChange7d: number;
+      percentChange12h: number;
+      percentChange15m: number;
+      percentChange24h: number;
+      percentChange30d: number;
+      percentChange30m: number;
+      percentCrompriceAth: number;
+      price: number;
+      volume24h: number;
+      volume24hChange24h: number;
+    };
+  };
+}
+
 const Coin = () => {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
 
   useEffect(() => {
     (async () => {
-      const infoData = await (
+      const infoData: InfoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
 
-      const priceData = await (
+      const priceData: PriceData = await (
         await fetch(`http://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
+
+      /**
+       *
+       * api 프로퍼티 타입 콘솔 디버깅 팁
+       * Object.keys()
+       * Object.values()
+       * Object.values().map(v => typeof v).join()
+       *
+       */
+      // console.log("info:", priceData.quotes);
+      // console.log(
+      //   "infoData:",
+      //   Object.values(priceData.quotes)
+      //     .map((v: any) => typeof v)
+      //     .join()
+      // );
     })();
   }, []);
 
