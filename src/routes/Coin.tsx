05730a3e2e23143b2b34adaa73c8d3ddbd5b18f7
bpthess <bpthess@naver.com>
2023-01-20 17:22:58 +0900

    coin api access

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { LoadingSpinner } from "./Coins";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 34px;
+  color: ${(props) => props.theme.accentColor};
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
-const Coin = () => {
-  const { coinId } = useParams<RouteParams>();
-  console.log("coinId:", coinId);
+interface RouteState {
+  name: string;
+}
 
-  return <h1>coin: {coinId}</h1>;
+const Coin = () => {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+
+      const priceData = await (
+        await fetch(`http://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || <LoadingSpinner />}</Title>
+      </Header>
+      {loading ? <LoadingSpinner /> : null}
+    </Container>
+  );
 };
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,12 +1,124 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
+  font-size: 34px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+const rotate = keyframes`
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+`;
+
+export const LoadingSpinner = styled.div`
+  border: 4px solid rgba(255, 255, 255, 0.5);
+  border-radius: 50%;
+  border-top-color: #fff;
+  font-size: 0;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 16px;
+  height: 16px;
+  transition: all 100ms;
+  animation: ${rotate} 1s linear infinite;
+`;
+
+interface CoinProps {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 const Coins = () => {
-  return <Title>코인</Title>;
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => {
+            return (
+              <Coin key={coin.id}>
+                <Link
+                  to={{
+                    pathname: `/${coin.id}`,
+                    state: { name: coin.name },
+                  }}
+                >
+                  <Img
+                    src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  />
+                  {coin.name} &rarr;
+                </Link>
+              </Coin>
+            );
+          })}
+        </CoinsList>
+      )}
+    </Container>
+  );
 };
 
 export default Coins;
